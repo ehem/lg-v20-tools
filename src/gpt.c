33bd9f58d2542e7e795d945ef6cec4cf936690b4
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2016-11-03 20:06:39 -0700

    Add checking of backup GPT to test code
    This is useful for debugging and comparing results.

--- src/gpt.c
+++ src/gpt.c
@@ -69,10 +69,14 @@ static void gpt_entry_raw2native(struct gpt_entry *, struct _gpt_entry *, iconv_
 
 
 #ifdef GPT_MAIN
+/******************* moved to display_gpt.c *******************/
 int main(int argc, char **argv)
 {
-	struct gpt_data *data;
+	struct gpt_data *data, *alt;
 	int f;
+	char buf0[37], buf1[37];
+	int i;
+
 	if(argc!=2) {
 		fprintf(stderr, "%s <GPT file>\n", argv[0]);
 		exit(128);
@@ -81,37 +85,42 @@ int main(int argc, char **argv)
 		fprintf(stderr, "%s: open() failed: %s\n", argv[0], strerror(errno));
 		exit(4);
 	}
-	if((data=readgpt(f, GPT_ANY))) {
-		char buf0[37], buf1[37];
-		int i;
-		const struct gpt_header *nat=&data->head;
-		printf("Found v%hu.%hu %s GPT in \"%s\" (%zd sector size)\n",
+	if(!(data=readgpt(f, GPT_ANY))) {
+		printf("No GPT found in \"%s\"\n", argv[1]);
+		return 1;
+	}
+
+	if((data->head.myLBA==1)&&(alt=readgpt(f, GPT_BACKUP))) {
+		if(comparegpt(data, alt)) printf("Found identical primary and backup GPTs\n");
+		else printf("Primary and backup GPTs differ!\n");
+		free(alt);
+	} else printf("Second GPT is absent!\n");
+
+	printf("Found v%hu.%hu %s GPT in \"%s\" (%zd sector size)\n",
 data->head.major, data->head.minor,
 data->head.myLBA==1?"primary":"backup", argv[1], data->blocksz);
-		uuid_unparse(data->head.diskUuid, buf0);
-		printf("device=%s\nmyLBA=%llu altLBA=%llu dataStart=%llu "
-"dataEnd=%llu\n\n", buf0, (unsigned long long)nat->myLBA,
-(unsigned long long)nat->altLBA, (unsigned long long)nat->dataStartLBA,
-(unsigned long long)nat->dataEndLBA);
+	uuid_unparse(data->head.diskUuid, buf0);
+	printf("device=%s\nmyLBA=%llu altLBA=%llu dataStart=%llu "
+"dataEnd=%llu\n\n", buf0, (unsigned long long)data->head.myLBA,
+(unsigned long long)data->head.altLBA,
+(unsigned long long)data->head.dataStartLBA,
+(unsigned long long)data->head.dataEndLBA);
 
-		for(i=0; i<nat->entryCount; ++i) {
-			if(uuid_is_null(data->entry[i].id)) {
-				printf("Name: <empty entry>\n");
-			} else {
-				uuid_unparse(data->entry[i].type, buf0);
-				uuid_unparse(data->entry[i].id, buf1);
-				printf("Name: \"%s\" start=%llu end=%llu\n"
+	for(i=0; i<data->head.entryCount; ++i) {
+		if(uuid_is_null(data->entry[i].id)) {
+			printf("Name: <empty entry>\n");
+		} else {
+			uuid_unparse(data->entry[i].type, buf0);
+			uuid_unparse(data->entry[i].id, buf1);
+			printf("Name: \"%s\" start=%llu end=%llu\n"
 "typ=%s id=%s\n", data->entry[i].name,
 (unsigned long long)data->entry[i].startLBA,
 (unsigned long long)data->entry[i].endLBA, buf0, buf1);
-			}
 		}
-
-		free(data);
-		return 0;
 	}
-	printf("No GPT found in \"%s\"\n", argv[1]);
-	return 1;
+
+	free(data);
+	return 0;
 }
 #endif
 
