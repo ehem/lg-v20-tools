145ff3703c2ded0072aa6eee64aaf8c408b42593
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2017-06-28 19:30:34 -0700

    Introduce H990 command-line fixer
    The way forward appears to be to add the extra command-line arguments to
    the boot image.  This allows us to merely need *one* kernel for all H990
    variants, rather than needing a bunch of kernels, each modifying the
    command-line in a different way.

--- src/Android.mk
+++ src/Android.mk
@@ -1,2 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fix-h990-modem
+LOCAL_SRC_FILES := fix-h990-modem.c
+LOCAL_CFLAGS := -Wall
+LOCAL_LDFLAGS := -static
+include $(BUILD_EXECUTABLE)
+
