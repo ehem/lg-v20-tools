e1601d615ebaf0c1911ba374731a4f1c4526d39b
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2017-07-24 19:47:44 -0700

    Implement test_kdzfile(), aiming to test whether a KDZ applies to the device
    There is great need to be able to test whether a given KDZ is likely to
    successfully apply to a given device.  Applying a bad KDZ is likely to
    cause a device not to boot ("brick").  The relationship between the KDZ
    and on-device structures is quite complicated.  This is looks plausible,
    I just hope my guesses were reasonable.

--- src/Android.mk
+++ src/Android.mk
@@ -21,7 +21,7 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := kdzwriter
-LOCAL_SRC_FILES := kdzwriter.c kdz.c md5.c
+LOCAL_SRC_FILES := kdzwriter.c kdz.c md5.c gpt.c
 LOCAL_CFLAGS := -Wall
 LOCAL_LDLIBS := -lz
 include $(BUILD_EXECUTABLE)
