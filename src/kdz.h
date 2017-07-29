7b0aa7334bf4ee9a3106e9c033950c019aa56b4f
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2017-07-28 20:36:05 -0700

    Implement write_kdzfile(), function for writing named KDZ chunks
    Simply seek out matching chunks, and then start writing them to media.
    Does compares with existing data to reduce flash fatigue and will
    discard/trim unused areas.

--- src/kdz.h
+++ src/kdz.h
@@ -118,5 +118,9 @@ extern int test_kdzfile(struct kdz_file *kdz);
 /* test and report state of device/KDZ */
 extern int report_kdzfile(struct kdz_file *kdz);
 
+/* (re)write the named flash slice, unless simulate */
+extern int write_kdzfile(struct kdz_file *kdz, const char *slice_name,
+bool simulate);
+
 #endif
 
