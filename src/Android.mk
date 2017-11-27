d182eb0c1072478b19e4b3a9fec4116ce554806b
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2017-11-27 14:13:18 -0800

    Rename fix-h990-cmdline to fix-h990-modem
    The end goal of this program has been to get the H990 modem operational.
    Modifying the boot image command-line was a means to this goal, it wasn't
    the actual goal.  Since the newer kernel allows this fixup to be done via
    other means, it seems appropriate to do a rename.

--- src/Android.mk
+++ src/Android.mk
@@ -29,8 +29,8 @@ include $(BUILD_EXECUTABLE)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := fix-h990-cmdline
-LOCAL_SRC_FILES := fix-h990-cmdline.c
+LOCAL_MODULE := fix-h990-modem
+LOCAL_SRC_FILES := fix-h990-modem.c
 LOCAL_CFLAGS := -Wall
 LOCAL_LDFLAGS := -static
 include $(BUILD_EXECUTABLE)
