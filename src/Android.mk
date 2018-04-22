974fd9b7483ad0986bcda9e4c71c4c010cad5d2a
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2018-04-22 00:42:26 -0700

    Creation of `rmOP`, tool to free up the OP area
    Perhaps the .5GB in /OP isn't a huge amount of space (my, how times
    change...), but it is potentially useful space.  Since it is otherwise
    filled with bloatware there seems to be little loss.

--- src/Android.mk
+++ src/Android.mk
@@ -1,2 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := rmOP
+LOCAL_SRC_FILES := rmOP.c gpt.c
+LOCAL_CFLAGS := -Wall
+LOCAL_LDFLAGS := -Wl,-dynamic-linker,/sbin/linker64
+LOCAL_LDLIBS := -lz
+include $(BUILD_EXECUTABLE)
+
