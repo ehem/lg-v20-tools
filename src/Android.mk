ddfe8ba41530ff504fac2ec93ff53f7769936d63
Elliott Mitchell <ehem+lg-v20-tools@m5p.com>
2018-12-21 21:05:40 -0800

    DEBUG: remove when tested for release

--- src/Android.mk
+++ src/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := split_system
 LOCAL_SRC_FILES := split_system.c gpt.c
-LOCAL_CFLAGS := -Wall
+LOCAL_CFLAGS := -Wall -DDEBUG
 LOCAL_LDFLAGS := -Wl,-dynamic-linker,/sbin/linker64
 LOCAL_LDLIBS := -lz
 include $(BUILD_EXECUTABLE)
