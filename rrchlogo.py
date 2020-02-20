918ef5cf86a418a1fc5e167c1bb680d060660df9
Elliott Mitchell <ehem+rrdecode@m5p.com>
2020-02-19 20:52:13 -0800

    Update Copyright notices
    As noted in the project history, these have been updated in 2020.  As
    such make sure the Copyright notices are accurate.

--- rrchlogo.py
+++ rrchlogo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright (C) 2019 Elliott Mitchell
+Copyright (C) 2019-2020 Elliott Mitchell
 
 	This program is free software: you can redistribute it and/or
 	modify it under the terms of the GNU General Public License as
